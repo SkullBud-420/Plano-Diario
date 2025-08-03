57a1eac1714363bb077ebcf68c31c16eb991dd21
SkullBud-420 <stalkmeta@gmail.com>
2025-08-03 12:53:35 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,8 @@
-const CACHE_NAME = 'skull-bud-diary-cache-v3'; // Versão do cache incrementada
+const CACHE_NAME = 'skull-bud-diary-cache-v2'; // Incrementamos a versão do cache
 const urlsToCache = [
   './',
   './Skull Bud 420 - Diário 0.1.html',
   './manifest.json',
-  './images/icons/icon-192x192.png',
-  './images/icons/icon-512x512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
